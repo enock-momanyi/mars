f4c8d0606b4debb0e8efb8a8f5c9fce2721a9c9e
atbftr <enockenm@gmail.com>
2022-01-11 17:56:23 +0300

    Redirect traffic to https

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
 const express = require('express');
 const path = require('path');
-const app = express();app.use(express.static(__dirname + '/dist/mars'));
+const app = express();
+app.use(requireHTTPS)
+app.use(express.static(__dirname + '/dist/mars'));
 app.get('/*', function(req,res) {res.sendFile(path.join(__dirname+'/dist/mars/index.html'));});
 app.listen(process.env.PORT || 8080);
